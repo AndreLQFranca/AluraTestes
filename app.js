6cd7f7c8604040e931551809ffaeddae7ca9f61d
unknown <andr3franca@gmail.com>
2023-06-16 22:14:32 -0300

    adicionando linhas

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("A aplicação está sendo conectada ao banco de dados");
+// mais um console log pra testar
+console.log("André fez esse commit agora...");
