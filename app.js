64ce978f444701a41bcfd65605198140bbc09726
unknown <andr3franca@gmail.com>
2023-06-16 22:27:37 -0300

    restaurando

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos");
-console.log("A aplicação está sendo conectada ao banco de dados");
-// mais um console log pra testar
-console.log("André fez esse commit agora...");
+console.log("Rodando o sistema de gerenciamento de jogos")
